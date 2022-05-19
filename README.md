28301c7cb969db2534b9723b4e6c22318a576036
anilgithu <101588507+anilgithu@users.noreply.github.com>
2022-05-19 12:10:36 +0530

    step 17

--- README.md
+++ README.md
@@ -73,3 +73,7 @@
 * create 2 columns
 * display Items list
 * create action column
+17. Complete Cart Screen
+    1. click handler for inc/dec item
+    2. click handler for remove item
+    3. click handler for checkout
--- amazona/Backend/server.js
+++ amazona/Backend/server.js
@@ -1,3 +1,31 @@
+// import express from 'express';
+// import data from './data.js';
+// const app = express();
+// // test
+// app.get('/api/products', (req, res) => {
+//   res.send(data.products);
+// });
+// app.get('/api/products/slug/:slug', (req, res) => {
+//   const product = data.products.find((x) => x.slug === req.params.slug);
+//   if (product) {
+//     res.send(product);
+//   } else {
+//     res.status(404).send({ message: 'Product Not Found' });
+//   }
+// });
+// app.get('/api/products/:id', (req, res) => {
+//   const product = data.products.find((x) => x._id === req.params.id);
+//   if (product) {
+//     res.send(product);
+//   } else {
+//     res.status(404).send({ message: 'Product Not Found' });
+//   }
+// });
+
+// const port = process.env.PORT || 5000;
+// app.listen(port, () => {
+//   console.log(`serve at http://localhost:${port}`);
+// });
 import express from 'express';
 import data from './data.js';
 const app = express();
@@ -5,6 +33,7 @@ const app = express();
 app.get('/api/products', (req, res) => {
   res.send(data.products);
 });
+
 app.get('/api/products/slug/:slug', (req, res) => {
   const product = data.products.find((x) => x.slug === req.params.slug);
   if (product) {
@@ -24,5 +53,6 @@ app.get('/api/products/:id', (req, res) => {
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
+  console.log(`Server at http://localhost:${port}`);
   console.log(`serve at http://localhost:${port}`);
 });
