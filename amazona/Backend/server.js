9a36e1dd2741cf50cbddfdb76a3dd594291f7094
anilgithu <101588507+anilgithu@users.noreply.github.com>
2022-05-21 10:52:54 +0530

    committ 20.22

--- amazona/Backend/server.js
+++ amazona/Backend/server.js
@@ -1,11 +1,50 @@
+// import express from 'express';
+// import data from './data.js';
+// import mongoose from 'mongoose';
+// import dotenv from 'dotenv';
+// import seedRouter from './routes/seedRoutes.js';
+// import productRouter from './routes/productRoutes.js';
+// import userRouter from './routes/userRoutes.js';
+
+// dotenv.config();
+
+// mongoose
+//   .connect(process.env.MONGODB_URI)
+//   .then(() => {
+//     console.log('connected to db');
+//   })
+//   .catch((err) => {
+//     console.log(err.message);
+//   });
+
+// const app = express();
+
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+
+// app.use('/api/seed', seedRouter);
+// app.use('/api/products', productRouter);
+// app.use('/api/users', userRouter);
+
+// app.use((err, req, res, next) => {
+//   res.status(500).send({ message: err.message });
+// });
+
+// const port = process.env.PORT || 5000;
+// app.listen(port, () => {
+//   console.log(`serve at http://localhost:${port}`);
+// });
+
 import express from 'express';
 import data from './data.js';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import seedRouter from './routes/seedRoutes.js';
 import productRouter from './routes/productRoutes.js';
+import userRouter from './routes/userRoutes.js';
 
 dotenv.config();
+
 mongoose
   .connect(process.env.MONGODB_URI)
   .then(() => {
@@ -16,6 +55,10 @@ mongoose
   });
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 // test
 app.get('/api/products', (req, res) => {
   res.send(data.products);
@@ -38,6 +81,11 @@ app.get('/api/products/:id', (req, res) => {
 });
 app.use('/api/seed', seedRouter);
 app.use('/api/products', productRouter);
+app.use('/api/users', userRouter);
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
