52d300680e1b6bf006ec80c7f67a0c0e1d20b4d1
anilgithu <101588507+anilgithu@users.noreply.github.com>
2022-05-21 10:37:18 +0530

    commit 20.21

--- amazona/Backend/data.js
+++ amazona/Backend/data.js
@@ -1,81 +1,22 @@
-// import bcrypt from 'bcryptjs';
-
-// const data = {
-//   users: [
-//     {
-//       name: 'Anil',
-//       email: 'admin@example.com',
-//       password: bcrypt.hashSync('123456'),
-//       isAdmin: true,
-//     },
-//     {
-//       name: 'John',
-//       email: 'user@example.com',
-//       password: bcrypt.hashSync('123456'),
-//       isAdmin: false,
-//     },
-//   ],
-//   products: [
-//     {
-//       // _id: '1',
-//       name: 'Nike Slim shirt',
-//       slug: 'nike-slim-shirt',
-//       category: 'Shirts',
-//       image: '/images/p1.jpg', // 679px × 829px
-//       price: 120,
-//       countInStock: 10,
-//       brand: 'Nike',
-//       rating: 4.5,
-//       numReviews: 10,
-//       description: 'high quality shirt',
-//     },
-//     {
-//       // _id: '2',
-//       name: 'Adidas Fit Shirt',
-//       slug: 'adidas-fit-shirt',
-//       category: 'Shirts',
-//       image: '/images/p2.jpg',
-//       price: 250,
-//       countInStock: 0,
-//       brand: 'Adidas',
-//       rating: 4.0,
-//       numReviews: 10,
-//       description: 'high quality product',
-//     },
-//     {
-//       // _id: '3',
-//       name: 'Nike Slim Pant',
-//       slug: 'nike-slim-pant',
-//       category: 'Pants',
-//       image: '/images/p3.jpg',
-//       price: 25,
-//       countInStock: 15,
-//       brand: 'Nike',
-//       rating: 4.5,
-//       numReviews: 14,
-//       description: 'high quality product',
-//     },
-//     {
-//       // _id: '4',
-//       name: 'Adidas Fit Pant',
-//       slug: 'adidas-fit-pant',
-//       category: 'Pants',
-//       image: '/images/p4.jpg',
-//       price: 65,
-//       countInStock: 5,
-//       brand: 'Puma',
-//       rating: 4.5,
-//       numReviews: 10,
-//       description: 'high quality product',
-//     },
-//   ],
-// };
-// export default data;
+import bcrypt from 'bcryptjs';
 
 const data = {
+  users: [
+    {
+      name: 'Basir',
+      email: 'admin@example.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: true,
+    },
+    {
+      name: 'John',
+      email: 'user@example.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: false,
+    },
+  ],
   products: [
     {
-      _id: '1',
       // _id: '1',
       name: 'Nike Slim shirt',
       slug: 'nike-slim-shirt',
@@ -89,7 +30,6 @@ const data = {
       description: 'high quality shirt',
     },
     {
-      _id: '2',
       // _id: '2',
       name: 'Adidas Fit Shirt',
       slug: 'adidas-fit-shirt',
@@ -103,7 +43,6 @@ const data = {
       description: 'high quality product',
     },
     {
-      _id: '3',
       // _id: '3',
       name: 'Nike Slim Pant',
       slug: 'nike-slim-pant',
@@ -117,7 +56,6 @@ const data = {
       description: 'high quality product',
     },
     {
-      _id: '4',
       // _id: '4',
       name: 'Adidas Fit Pant',
       slug: 'adidas-fit-pant',
